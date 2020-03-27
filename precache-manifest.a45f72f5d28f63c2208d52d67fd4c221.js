54a5b6cdf8e26b7e318b87c03723cb0a13b0a651
Skorzynski <Kamil.Skorzynski@jacobs.com>
2020-03-27 17:27:16 +0100

    Updates

---
+++ precache-manifest.a45f72f5d28f63c2208d52d67fd4c221.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c0784f4b04824850f33",
+    "url": "/To-do-app/static/css/main.c95758e8.chunk.css"
+  },
+  {
+    "revision": "4c0784f4b04824850f33",
+    "url": "/To-do-app/static/js/main.85cf19ae.chunk.js"
+  },
+  {
+    "revision": "379b5dab363eab341e7f",
+    "url": "/To-do-app/static/js/runtime~main.1c4534f2.js"
+  },
+  {
+    "revision": "a06eb0cf4958763fc45e",
+    "url": "/To-do-app/static/js/2.83b629c2.chunk.js"
+  },
+  {
+    "revision": "2a03fb3ef1cb982b9b3b9f5b9fece0c8",
+    "url": "/To-do-app/index.html"
+  }
+];
